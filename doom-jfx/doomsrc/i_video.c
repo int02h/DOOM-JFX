74fccf043ff3c7fc41a5d6ab710a5878dcfcb334
Daniil Popov <int02h@gmail.com>
2025-02-19 15:46:03 +0200

    Patched sources of linuxdoom to make it compile on macOS arm64 machine.

---
+++ doom-jfx/doomsrc/i_video.c
@@ -0,0 +1,63 @@
+// Emacs style mode select   -*- C++ -*-
+//-----------------------------------------------------------------------------
+//
+// $Id:$
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+//
+// This source is available for distribution and/or modification
+// only under the terms of the DOOM Source Code License as
+// published by id Software. All rights reserved.
+//
+// The source is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
+// for more details.
+//
+// $Log:$
+//
+// DESCRIPTION:
+//	DOOM graphics stuff for X11, UNIX.
+//
+//-----------------------------------------------------------------------------
+
+static const char
+rcsid[] = "$Id: i_x.c,v 1.6 1997/02/03 22:45:10 b1 Exp $";
+
+#include "doomtype.h"
+
+void I_ShutdownGraphics(void)
+{
+}
+
+void I_StartFrame (void)
+{
+}
+
+void I_GetEvent(void)
+{
+}
+
+void I_StartTic (void)
+{
+}
+
+void I_UpdateNoBlit (void)
+{
+}
+
+void I_FinishUpdate (void)
+{
+}
+
+void I_ReadScreen (byte* scr)
+{
+
+}
+void I_SetPalette (byte* palette)
+{
+}
+
+void I_InitGraphics(void)
+{
+}
